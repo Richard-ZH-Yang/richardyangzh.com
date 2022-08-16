e4bf18f9db277aa68266d8d5db6ec5372b78dc79
Richard <richardyangzh@gmail.com>
2022-08-16 15:32:09 -0400

    fix

--- components/Main.jsx
+++ components/Main.jsx
@@ -16,7 +16,7 @@ const Main = () => {
           </h1>
           <h1 className='py-2 text-gray-700'>A Software Development Engineer</h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I'm a fourth-year student at UBC majoring in Computer Science with a minor in Data Science. I am currently working as a Backend Developer Intern at IBM and previously worked as a Full Stack Developer Intern at CAE.
+            I&apos;m a fourth-year student at UBC majoring in Computer Science with a minor in Data Science. I am currently working as a Backend Developer Intern at IBM and previously worked as a Full Stack Developer Intern at CAE.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <a
