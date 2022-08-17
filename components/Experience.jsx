60a40ec6f7cd8f34aba6280b47a3d955f23b9898
Richard <richardyangzh@gmail.com>
2022-08-16 21:57:32 -0400

    remove wrong npm packages

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -3,7 +3,7 @@ import ExperienceItem from './ExperienceItem';
 
 const Experience = () => {
   return (
-    <div id='projects' className='w-full'>
+    <div id='experience' className='w-full'>
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Experience
