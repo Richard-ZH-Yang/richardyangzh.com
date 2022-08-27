9d49282200cf5a7cdfa801a30b79d02dbc07ee55
Richard <richardyangzh@gmail.com>
2022-08-27 00:41:51 -0400

    align the names in testimonials

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -4,17 +4,17 @@ const Tesimonials = () => {
   const data = [
     {
       id: 1,
-      name: "Gordon Wong",
-      title: "Senior Software Developer @ CAE",
-      desc:
-        "Richard was very receptive to comments and has exhibited a passion to learn new technologies. He has also shown that he is willing to go far and beyond to get something done.",
-    },
-    {
-      id: 2,
       name: "Zhibo Zhang",
       title: "Senior Software Engineering Manager @ IBM",
       desc:
         "Richard is a brilliant and gifted software developer who joined my team as a backend developer intern. During his internship, he has not only shown his great technical skills, but also has brought a lot of positive energies to the team. He is a fast learner, a challenge seeker, very proactive, self-motivated, and goal-oriented. Richard was able to contribute to a new feature after three weeks of joining the team, this is very impressive. When he approached the implementation of the new feature, he also thoroughly investigated the related code areas and fixed bugs as discovered. When investigation tasks are assigned to him, Richard can come up with PoCs and great presentations in a timely manner, and share the knowledge with the rest of the team. Overall, Richard is a great developer to work with and I wish him all the best in his future!",
+    },
+    {
+      id: 2,
+      name: "Gordon Wong",
+      title: "Senior Software Developer @ CAE",
+      desc:
+        "Richard was very receptive to comments and has exhibited a passion to learn new technologies. He has also shown that he is willing to go far and beyond to get something done. Being someone in his first work term, Richard has exceeded all the expectations.\n\n\n\n\n\n\n\n\n\n\n\n",
     },
   ];
 
@@ -27,7 +27,7 @@ const Tesimonials = () => {
         </h2>
         <div className='grid md:grid-cols-2 gap-8'>
           {data.map((d) => (
-            <div key={d.id} className="relative items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group" style={{ textAlign: 'center'}}>
+            <div key={d.id} className="relative items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group" style={{ textAlign: 'center', whiteSpace: 'pre-wrap' }}>
               <p style={{
                 width: '100%',
                 padding: '0 50px',
